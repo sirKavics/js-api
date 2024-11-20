44d219a9cea20023a2f2ba0eda487adc1518da08
sirkavics <falf555@gmail.com>
2024-11-20 01:13:37 -0500

    js fetching posts from user id and search on change into dynamic HTML

--- user.js
+++ user.js
@@ -1,15 +1,19 @@
 const postListEl = document.querySelector(".post-list");
+const id = localStorage.getItem("id");
 
-async function main() {
-  const id = localStorage.getItem("id");
-  const posts = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?userId=${id}`
-  );
+async function onSearchChange(event) {
+  const id = event.target.value;
+  renderPosts(id)
+}
+
+async function renderPosts(id) {
+  const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
   const postsData = await posts.json();
-  console.log(postsData);
+  postListEl.innerHTML = postsData.map(post => postHTML(post) ).join('');
+}
 
-  postListEl.innerHTML = postsData.map(
-    (post) => `
+function postHTML(post) {
+    return `
         <div class="post">
         <div class="post__title">
             ${post.title}
@@ -18,7 +22,7 @@ async function main() {
             ${post.body}
         </p>
         </div>`
-  ).join('');
+    
 }
 
-main();
+renderPosts(id);
